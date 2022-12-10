b949a66e3828a82e10629a1f3f17c0307267df69
akshaysharma44p <86362062+akshaysharma44p@users.noreply.github.com>
2022-12-10 11:09:02 +0530

    akshay

---
+++ fun.js
@@ -0,0 +1,13 @@
+document.write(
+    "Hello JavaScript"
+)
+function msg(){  
+    alert("Hello Javatpoint");  
+   } 
+   var q=20;
+   var p=30;
+   var n=p+q;
+   document.write(n);
+   function abc(){  
+    var x=10; 
+    }  
